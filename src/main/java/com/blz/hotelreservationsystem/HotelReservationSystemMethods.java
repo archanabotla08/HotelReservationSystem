c265770b9ea93d4eefc85ec35b3e95b39acf5aa2
Nisha271 <bukkenisha@gmail.com>
2020-11-11 16:41:44 +0530

    [Archana] Added: UC2 Ability to find the cheapest hotel

--- src/main/java/com/blz/hotelreservationsystem/HotelReservationSystemMethods.java
+++ src/main/java/com/blz/hotelreservationsystem/HotelReservationSystemMethods.java
@@ -1,24 +1,56 @@
 package com.blz.hotelreservationsystem;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-
+import java.util.Comparator;
 
 
 public class HotelReservationSystemMethods {
-	
-	private static ArrayList<HotelReservationSystem> hotelDetailsList = new ArrayList<>();
-	
-	// UseCase: 1
-		public static void addHotel(String hotelName, int regularRates) {
-			HotelReservationSystem hotelReservationSystem = new HotelReservationSystem(hotelName, regularRates);
-			hotelDetailsList.add(hotelReservationSystem);
-		}
 
-		public static int countHotels() {
-			return hotelDetailsList.size();
+	private static ArrayList<HotelReservationSystem> hotelDetailsList = new ArrayList<>();
+
+	// UseCase: 1
+	public static void addHotel(String hotelName, int regularRates) {
+		HotelReservationSystem hotelReservationSystem = new HotelReservationSystem(hotelName, regularRates);
+		hotelDetailsList.add(hotelReservationSystem);
+	}
+
+	public static int countHotels() {
+		return hotelDetailsList.size();
+	}
+
+	// UseCase: 2
+	public static LocalDate convertStringToDate(String dateString) {
+		LocalDate date = null;
+		DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("ddMMMyyyy");
+		date = LocalDate.parse(dateString, dateTimeFormat);
+		return date;
+	}
+	
+	public static String getCheapestHotel(String input_1, String input_2) {
+		LocalDate dateInput_1 = convertStringToDate(input_1);
+		LocalDate dateInput_2 = convertStringToDate(input_2);
+		int bookingDays = 0;
+		bookingDays = (int) ChronoUnit.DAYS.between(dateInput_1, dateInput_2) + 1;
+		hotelDetailsList.add(new HotelReservationSystem("LakeWood", 110));
+		hotelDetailsList.add(new HotelReservationSystem("BridgeWood", 150));
+		hotelDetailsList.add(new HotelReservationSystem("RidgeWood", 220));
+		for (int i = 0; i < hotelDetailsList.size(); i++) {
+			int value = hotelDetailsList.get(i).getRegularRates() * (bookingDays);
+			hotelDetailsList.get(i).setRegularRates(value);
 		}
+		String hotelName = hotelDetailsList.stream().min(Comparator.comparing(HotelReservationSystem::getRegularRates)).get()
+				.getHotelName();
+		int cheapestRate = hotelDetailsList.stream().min(Comparator.comparing(HotelReservationSystem::getRegularRates)).get()
+				.getRegularRates();
+
+		return hotelName + " " + cheapestRate;
+	}
 	public static void main(String[] args) {
-		//System.out.println("Welcome Message");
+		// System.out.println("Welcome Message");
 	}
 
 }
--- src/test/java/com/blz/hotelreservationsystem/HotelReservationSystemTest.java
+++ src/test/java/com/blz/hotelreservationsystem/HotelReservationSystemTest.java
@@ -4,16 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class HotelReservationSystemTest {
 
 	// Test UC:1
-		@Test
-		public void given3Hotels_WhenAddedToHotelMap_SizeOfMapIs3() {
-			HotelReservationSystemMethods.addHotel("LakeWood", 110);
-			HotelReservationSystemMethods.addHotel("Bridgewood", 150);
-			HotelReservationSystemMethods.addHotel("Ridgewood", 220);
-			assertEquals(3, HotelReservationSystemMethods.countHotels());
-		}
+	@Test
+	public void given3Hotels_WhenAddedToHotelMap_SizeOfMapIs3() {
+		assertEquals(3, HotelReservationSystemMethods.countHotels());
+	}
+
+	// Test UC:1
+	@Test
+	public void givenHotels_GetCheapestHotel() {
+		String result = HotelReservationSystemMethods.getCheapestHotel("10Sep2020", "11Sep2020");
+		assertEquals("LakeWood 220", result);
+	}
 
 }

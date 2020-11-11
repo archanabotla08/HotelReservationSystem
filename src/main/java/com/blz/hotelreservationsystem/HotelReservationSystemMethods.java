7696cb693678a2668031fdda6e31414394a1cf25
Nisha271 <bukkenisha@gmail.com>
2020-11-11 18:14:16 +0530

    [Archana] Added: UC6 Ability to get cheapest best rated hotel

--- src/main/java/com/blz/hotelreservationsystem/HotelReservationSystemMethods.java
+++ src/main/java/com/blz/hotelreservationsystem/HotelReservationSystemMethods.java
@@ -7,17 +7,21 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class HotelReservationSystemMethods {
 
+	private static int cheapestRate;
 	private static ArrayList<HotelReservationSystem> hotelDetailsList = new ArrayList<>();
+	static Map<String, Integer> hotelMap = new HashMap<>();
+	public static ArrayList<String> hotelList = new ArrayList<String>();
 	public static ArrayList<String> strDays = new ArrayList<String>(
 			Arrays.asList("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"));
 
-	public static void addHotel(String hotelName, int weekDayRates, int weekEndDayRates,int ratings) {
+	public static void addHotel(String hotelName, int weekDayRates, int weekEndDayRates, int ratings) {
 		HotelReservationSystem hotelReservationSystem = new HotelReservationSystem(hotelName, weekDayRates,
-				weekEndDayRates,ratings);
+				weekEndDayRates, ratings);
 		hotelDetailsList.add(hotelReservationSystem);
 	}
 
@@ -31,21 +35,22 @@ public class HotelReservationSystemMethods {
 		date = LocalDate.parse(dateString, dateTimeFormat);
 		return date;
 	}
+
 	public static DayOfWeek getDays(LocalDate date) {
 		DayOfWeek day = null;
 		day = DayOfWeek.from(date);
 		return day;
 	}
 
-	public static String getCheapestHotel(String input_1, String input_2) {
+	public static ArrayList<String> getCheapestHotel(String input_1, String input_2) {
 		LocalDate dateInput_1 = convertStringToDate(input_1);
 		LocalDate dateInput_2 = convertStringToDate(input_2);
 		DayOfWeek dayOfWeek_1 = getDays(dateInput_1);
 		DayOfWeek dayOfWeek_2 = getDays(dateInput_2);
 
-		hotelDetailsList.add(new HotelReservationSystem("LakeWood", 110, 90,3));
-		hotelDetailsList.add(new HotelReservationSystem("BridgeWood", 150, 50,4));
-		hotelDetailsList.add(new HotelReservationSystem("RidgeWood", 220, 150,5));
+		hotelDetailsList.add(new HotelReservationSystem("LakeWood", 110, 90, 3));
+		hotelDetailsList.add(new HotelReservationSystem("BridgeWood", 150, 50, 4));
+		hotelDetailsList.add(new HotelReservationSystem("RidgeWood", 220, 150, 5));
 		int rent = 0;
 		for (int i = 0; i < hotelDetailsList.size(); i++) {
 			if (strDays.contains(dayOfWeek_1) || strDays.contains(dayOfWeek_2)) {
@@ -63,13 +68,32 @@ public class HotelReservationSystemMethods {
 		for (int i = 0; i < hotelDetailsList.size(); i++) {
 			int temp = hotelDetailsList.get(i).getWeekDayRates() + hotelDetailsList.get(i).getWeekEndDayRates();
 			hotelDetailsList.get(i).setWeekDayRates(temp);
+
 		}
+		cheapestRate = hotelDetailsList.stream().min(Comparator.comparing(HotelReservationSystem::getWeekDayRates))
+				.get().getWeekDayRates();
+
 		String hotelName = hotelDetailsList.stream().min(Comparator.comparing(HotelReservationSystem::getWeekDayRates)).get()
 				.getHotelName();
-		int cheapestRate = hotelDetailsList.stream().min(Comparator.comparing(HotelReservationSystem::getWeekDayRates)).get()
-				.getWeekDayRates();
+		
+		for (HotelReservationSystem hotel : hotelDetailsList) {
+			hotelList.add(hotel.getHotelName());
+			hotelMap.put(hotel.getHotelName(), hotel.getRatings());
+		}
 
-		return hotelName + " Total Rate $: " + cheapestRate;
+		return hotelList;
+	}
+
+	public static String cheapestBestRatedHotel(String input_1, String input_2) {
+		getCheapestHotel(input_1, input_2);
+		Map.Entry<String, Integer> hotelRating = null;
+		for (Map.Entry<String, Integer> entry : hotelMap.entrySet()) {
+			if (hotelRating == null || entry.getValue().compareTo(hotelRating.getValue()) > 0) {
+				hotelRating = entry;
+				
+			}
+		}
+		return hotelRating.getKey() + ", Rating: " + hotelRating.getValue() + " and Total Rates: $" + cheapestRate;
 	}
 
 	public static void main(String[] args) {
--- src/test/java/com/blz/hotelreservationsystem/HotelReservationSystemTest.java
+++ src/test/java/com/blz/hotelreservationsystem/HotelReservationSystemTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
 public class HotelReservationSystemTest {
 
 	@Test
@@ -12,6 +13,11 @@ public class HotelReservationSystemTest {
 		HotelReservationSystemMethods.addHotel("Bridgewood", 150, 50,4);
 		HotelReservationSystemMethods.addHotel("Ridgewood", 220, 150,5);
 		assertEquals(3, HotelReservationSystemMethods.countHotels());
+	}
+	@Test
+	public void given2Dates_GetCheapestHotelByRatings() {
+		String result = HotelReservationSystemMethods.cheapestBestRatedHotel("11Sep2020", "12Sep2020");
+		assertEquals("BridgeWood" + ", Rating: " + 4 + " and Total Rates: $200", result);
 	}
 
 }
